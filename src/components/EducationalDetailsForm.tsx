1796ee731c2a9a641289f1a1aeb503d24eed675b
Trevor Nganyi <nganyi8@gmail.com>
2022-06-02 01:25:52 +0300

    Trevor changes made on 2 june

--- src/components/EducationalDetailsForm.tsx
+++ src/components/EducationalDetailsForm.tsx
@@ -1,6 +1,7 @@
-import { IonButton, IonCol, IonContent, IonHeader, IonInput, IonItem, IonLabel, IonRow, IonSelect, IonSelectOption } from "@ionic/react"
+import { IonButton, IonCheckbox, IonCol, IonContent, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonRow, IonSelect, IonSelectOption } from "@ionic/react"
+import { checkbox } from "ionicons/icons"
 import React, { useRef, useState } from "react"
-
+import './EducationalDetailsForm.css';
 let primaryhost:any= "https://www.dmzee.co.za/"
 
 
@@ -9,15 +10,29 @@ const EducationalDetailsForm = (props:any)=>{
     var ddlLevel:any                        =useRef<HTMLIonInputElement>(null)
     var ddlGrade:any                        =useRef<HTMLIonInputElement>(null)
     var ddlQualification:any                =useRef<HTMLIonInputElement>(null)
+    var ddlSubject:any                      =useRef<HTMLIonInputElement>(null)
     var contact_id:any = 0
 
-    const [getLevel,setLevel]               =useState<HTMLIonSelectOptionElement>()
-    const[getCertificate,setCertificate]    =useState<HTMLIonSelectOptionElement>()
-    const[getQual,setQual]                  =useState<HTMLIonSelectOptionElement>()
-    const [getGrade,setGrade]               =useState<HTMLIonSelectOptionElement>()
-    
-    
+    const [getLevel,setLevel]                       =useState<HTMLIonSelectOptionElement>()
+    const[getCertificate,setCertificate]            =useState<HTMLIonSelectOptionElement>()
+    const[getQual,setQual]                          =useState<HTMLIonSelectOptionElement>()
+    const [getGrade,setGrade]                       =useState<HTMLIonSelectOptionElement>()
+    const [getSubjectList,setSubjectList]           =useState<HTMLIonSelectOptionElement>()
+    const [getSubjectParent,setSubjectParent]       =useState<HTMLIonSelectOptionElement>()
+    const [getClick,setClick]                       =useState<boolean>(false)
+    const [getId,setId]                             =useState<any>("0")
+    const [getIsChecked,setIsChecked]                    =useState<string>('text-grey')
     const[getQualifications,setQualifications] = useState<HTMLIonRowElement>()
+
+    const setSelected=(id:any)=>{
+        var x:any = document.getElementsByClassName("row_"+id)
+        x[0].addClass("text-green")
+    }
+    const resetSelected=(id:any)=>{
+        var x:any = document.getElementsByClassName("row_"+id)
+        x[0].removeClass("text-green")
+        x[0].addClass("text-grey")
+    }
     const callListCert = (parent_id:any) =>{
         var options:any=[]
         fetch(primaryhost+"education/dbconnect/select.jsp?edu=select_list"+
@@ -87,6 +102,29 @@ const EducationalDetailsForm = (props:any)=>{
             
         })
     }
+    const callListSubject = (parent_id:any) =>{
+        var options:any=[]
+        fetch(primaryhost+"education/dbconnect/select.jsp?edu=select_list"+
+        "&parent_id="+parent_id+
+        "&list_id="+0,
+        {
+            headers:{"content-type":"application/x-www-form-urlencoded; charset=UTF-8"}
+        }
+        )
+        .then(response => response.json())
+        .then(data => {
+            options.push(data.data)
+            // if (parent_id==51){
+                var list:any=options[0].map((x:any,i:number)=>{
+                    return(
+                        <IonSelectOption key={i} value={x.id}>{x.list_desc}</IonSelectOption>
+                    )
+                })
+                setSubjectList(list)
+            // }
+            
+        })
+    }
     const callQualification = () =>{
         var options:any=[]
         fetch(primaryhost+"education/dbconnect/dbdynamic.jsp?dbo=select_qualification"+
@@ -101,19 +139,25 @@ const EducationalDetailsForm = (props:any)=>{
             console.log(data)
             options.push(data.data)
             var headers:any=(<IonRow className="ion-text-bold size-18">
+                        <IonCol size='1'>Select</IonCol>
                         <IonCol>ID</IonCol>
                         <IonCol>Qualification Type</IonCol>
                         <IonCol>Level</IonCol>
+                        <IonCol>Subject</IonCol>
                         <IonCol>Grade</IonCol>
                      </IonRow>)
             var combined:any=[]
             var list:any=options[0].map((x:any, i:number)=>{
                 return(
-                    <IonRow key = {i}>
+                    <IonRow key = {i} className={'row_'+x.id}>
+                        <IonCol  className="hover" size="1">
+                            <IonCheckbox onIonChange = {(e)=>{alert(e.detail.checked);if(e.detail.checked){setSelected(x.id)}else{resetSelected(x.id)}}}></IonCheckbox>
+                        </IonCol>
                         <IonCol>{x.id}</IonCol>
-                        <IonCol>{x.qualification_id}</IonCol>
-                        <IonCol>{x.level_id}</IonCol>
-                        <IonCol>{x.grade_id}</IonCol>
+                        <IonCol>{x.qualification}</IonCol>
+                        <IonCol>{x.level}</IonCol>
+                        <IonCol>{x.subject}</IonCol>
+                        <IonCol>{x.grade}</IonCol>
                     </IonRow>
                 )
             })
@@ -149,7 +193,8 @@ const EducationalDetailsForm = (props:any)=>{
         "&contact_id="+props.userProperties.contact_id+
         "&qualification_id="+ddlQualification.current!.value+
         "&level_id="+ddlLevel.current!.value+
-        "&grade_id="+ddlGrade.current!.value,
+        "&grade_id="+ddlGrade.current!.value+
+        "&subject_id="+ddlSubject.current!.value,
         {
             headers:{"content-type":"application/x-www-form-urlencoded; charset=UTF-8"}
         }
@@ -168,16 +213,20 @@ const EducationalDetailsForm = (props:any)=>{
     const callGrade=()=>{
         callListGrade(126)
     }
+    const callSubject=()=>{
+        callListSubject(getSubjectParent)
+    }
 
     React.useEffect(()=>{
         callLevel()
         callCert()
         callGrade()
         callQualification()
+        callSubject()
     },[])
     
     return(
-        <div className="education-box">
+        <div className="education-box ion-heading">
             <IonHeader> 
                 <IonRow>
                     <IonCol className="ion-text-bold size-20">Education Details</IonCol>
@@ -192,7 +241,13 @@ const EducationalDetailsForm = (props:any)=>{
                     <IonCol size = "2">
                         <IonItem>
                             <IonLabel position= "floating">Level</IonLabel>
-                            <IonSelect ref={ddlLevel}>{getLevel}</IonSelect>
+                            <IonSelect onIonChange={(e)=>{setSubjectParent(e.detail.value);callListSubject(e.detail.value)}} ref={ddlLevel}>{getLevel}</IonSelect>
+                        </IonItem>
+                    </IonCol>
+                    <IonCol size = "2">
+                        <IonItem>
+                            <IonLabel position= "floating">Subject</IonLabel>
+                            <IonSelect  ref={ddlSubject}>{getSubjectList}</IonSelect>
                         </IonItem>
                     </IonCol>
                     <IonCol size = "2">
